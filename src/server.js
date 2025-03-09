774600095f05227c497c2cddc9be120aefb86051
nhatminhnt <nhatminhnt1522@gmail.com>
2025-03-09 15:50:33 +0700

    template engine va static file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
-const express = require('express') //common js
+const { config } = require('dotenv');
+const express = require('express'); //common js
 const path = require('path'); //common js
+require('dotenv').config(); 
 //import express from 'express'; //es modules
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT;
+const hostname = process.env.HOST_NAME;
 
 //config template engine
-app.set('views', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+
+// config static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World vs Hoi Dan IT $ Eric!')
@@ -22,6 +28,6 @@ app.get('/hoidanIT', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
